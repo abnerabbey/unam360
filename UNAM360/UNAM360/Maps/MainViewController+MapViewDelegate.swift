ad1e8a88ce01127381a68838d6206d68a6ee7876
Carlos Daniel Hernandez Chauteco <gamercarlos1530@gmail.com>
2019-05-13 19:54:40 -0500

    -added Delegate File

---
+++ UNAM360/UNAM360/Maps/MainViewController+MapViewDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController+MapViewDelegate.swift
+//  UNAM360
+//
+//  Created by Carlos Daniel Hernandez Chauteco on 5/13/19.
+//  Copyright © 2019 IOSDevLab. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+extension ViewController: MKMapViewDelegate{
+    
+}
